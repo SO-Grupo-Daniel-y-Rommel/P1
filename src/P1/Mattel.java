7a406813fce315fafc2eea1b81c823461e1c4e16
Daniel-Montilla-UNIMET <d.montilla@correo.unimet.edu.ve>
2021-05-17 15:42:44 -0400

    arreglos a estructura semaforos

--- src/P1/Mattel.java
+++ src/P1/Mattel.java
@@ -13,7 +13,7 @@ public class Mattel {
     public static int segundos_por_dia;
     public static int dias_entre_despachos;
 
-    public static int cantidad_inicial_ensambladores;
+    public static int cantidad_ensambladores;
     public static int cantidad_maxima_ensambladores;
     
     /* Formato de arreglos:
@@ -23,11 +23,11 @@ public class Mattel {
      * i = 3 -> Cuerpos
      */
     public static int[] cantidades_almacen = new int[4];
-    public static int[] cantidad_inicial_productores = new int[4];
+    public static int[] cantidad_productores = new int[4];
     public static int[] cantidad_maxima_productores = new int[4];
     
-    public static Productor[] productores;
-    public static Ensamblador[] ensambladores;
+    public static volatile Productor[] productores;
+    public static volatile Ensamblador[] ensambladores;
     
     public static void main(String[] args) {
         
@@ -36,33 +36,38 @@ public class Mattel {
         Mattel.segundos_por_dia = 20;
         Mattel.dias_entre_despachos = 10;
 
-        Mattel.cantidad_inicial_ensambladores = 1;
+        Mattel.cantidad_ensambladores = 1;
         Mattel.cantidad_maxima_ensambladores = 5;
         
         // Para Boton i = 0
         Mattel.cantidades_almacen[0] = 60;
-        Mattel.cantidad_inicial_productores[0] = 1;
+        Mattel.cantidad_productores[0] = 1;
         Mattel.cantidad_maxima_productores[0] = 4;
         
         // Para Brazos i = 1
         Mattel.cantidades_almacen[1] = 40;
-        Mattel.cantidad_inicial_productores[1] = 1;
+        Mattel.cantidad_productores[1] = 1;
         Mattel.cantidad_maxima_productores[1] = 5;
         
         // Para Piernas i = 2
         Mattel.cantidades_almacen[2] = 36;
-        Mattel.cantidad_inicial_productores[2] = 1;
+        Mattel.cantidad_productores[2] = 1;
          Mattel.cantidad_maxima_productores[2] = 4;
         
         // Para Cuerpos i = 3
         Mattel.cantidades_almacen[3] = 15;
-        Mattel.cantidad_inicial_productores[3] = 1;
+        Mattel.cantidad_productores[3] = 1;
         Mattel.cantidad_maxima_productores[3] = 4;
 
         
         // TODO: Fill los arreglos con los datos dados
 //        Mattel.productores;
 //        Mattel.ensambladores;
+
+        // Rutinas para comenzar cada thread
+        for (int i = 0; i < Mattel.productores.length; i++) {
+            Mattel.productores[i].start();
+        }
         
     }
 }
